189c0d94e013d9850dede8e9119608397e51394e
twktheainur <andon.tchechmedjiev@mines-ales.fr>
2023-08-07 16:38:27 +0200

    Updated token categorization

--- token_categorization.py
+++ token_categorization.py
@@ -10,17 +10,16 @@ from torch import pdist, nn, mm
 
 from tqdm import tqdm
 
-from sentence_transformers import SentenceTransformer, util
 
-model_name = 'paraphrase-multilingual-mpnet-base-v2'
+model_name = "paraphrase-multilingual-mpnet-base-v2"
 threshold = 0.2
 
 from redis import Redis
 
-redis = Redis(decode_responses=True, port=6380)
+redis = Redis(decode_responses=True, port=6379)
 
 
-def get(url: str, headers, timeout= None):
+def get(url: str, headers, timeout=None):
     page_text = redis.get(url)
     try:
         if not page_text:
@@ -71,56 +70,101 @@ def cluster_similar_expressions(ordered_dict, model, threshold=0.4):
     return merged_ordered_dict
 
 
-sentences_df = pandas.read_csv(
-    "extracted_knowlege/sentences_corrections.tsv",
-    sep='\t', header=None)
+# sentences_df = pandas.read_csv(
+#     "extracted_knowlege/Gauss_303_.csv", sep=",", header=None
+# )
 
-corpus = sentences_df.iloc[:, 1].tolist()
+# corpus = sentences_df.iloc[:, 0].tolist()
 
+with open("extracted_knowlege/h3D_preds_refs.csv", "r") as f:
+    lines = f.readlines()
+refs = []
+preds = []
+
+for id, line in enumerate(lines):
+    rp = line.replace("\n", "").split(",")
+    local_refs = rp[1:]
+    pred = rp[0]
+    for i in range(len(local_refs)):
+        refs.append(local_refs[i])
+        preds.append(pred)
+
+corpus = preds
 import requests
 
 matched_conditions = {}
-conditions = {
-    'verbs': (lambda x: x['vn'] == 'Verb', lambda x: x['text']),
-    'manners': (lambda x: x['pb'] == 'AM-MNR', lambda x: x['text']),
-    'directions': (
-        lambda x: x['pb'] == 'AM-DIR' or 'direction' in x['description'] or 'path' in x['description'],
-        lambda x: x['text'])
+span_conditions = {
+    "agents": (lambda x: x["vn"] == "Agent", lambda x: x["text"]),
+    "actions": (lambda x: x["vn"] == "Verb", lambda x: x["text"]),
+    "manners": (lambda x: x["pb"] == "AM-MNR", lambda x: x["text"]),
+    "directions": (
+        lambda x: x["pb"] == "AM-DIR"
+        or ("direction" in x["description"] and "AM-LVB" not in x["pb"])
+        or "path" in x["description"],
+        lambda x: x["text"],
+    ),
 }
 limit = 20
-for sentence in tqdm(corpus):
+output_document = []
+for index, sentence in tqdm(list(enumerate(corpus))):
+    sentence = sentence.replace("<eos>", ".")
     # print("Sentence: "+ sentence)
-    url = f"http://localhost:8080/predict/semantics?{urlencode({'utterance': sentence})}"
+    url = (
+        f"http://localhost:8080/predict/semantics?{urlencode({'utterance': sentence})}"
+    )
     response = get(url, {})
-    response = json.loads(response)
+    if response is not None:
+        response = json.loads(response)
+        annotation = {}
+        senses = []
+        if "props" in response:
+            for prop in response["props"]:
+                if "performance" not in prop["sense"]:
+                    spans = prop["spans"]
+                    for span in spans:
+                        for condition, value in span_conditions.items():
+                            if value[0](span):
+                                if condition not in annotation:
+                                    annotation[condition] = []
 
-    if 'props' in response:
-        for prop in response['props']:
-            spans = prop['spans']
-            for span in spans:
-                for condition in conditions:
-                    if conditions[condition][0](span):
-                        if condition not in matched_conditions:
-                            matched_conditions[condition] = []
-                        matched_conditions[condition].append((conditions[condition][1](span),prop['sense']))
+                                annotation[condition].append(
+                                    (
+                                        span_conditions[condition][1](span),
+                                        span["start"],
+                                        span["end"],
+                                    )
+                                )
+                    senses.append(prop["sense"])
+            output_document.append(
+                {
+                    "sentence": sentence,
+                    "sentence_index": index,
+                    "senses": prop["sense"],
+                    "annotations": annotation,
+                }
+            )
+
+with open("extracted_knowlege/semantic_segmentation_jit.json", "w") as f:
+    json.dump(output_document, f, indent=2)
     # limit -= 1
     # if limit == 0:
     #     break
-model = SentenceTransformer(model_name)
+# model = SentenceTransformer(model_name)
 
-for condition in matched_conditions:
-    print(f"****{condition}****")
-    cond_list = matched_conditions[condition]
-    cond_histogram = dict(collections.Counter(cond_list))
-    words_col = []
-    counts_col = []
-    for entry in cond_histogram.items():
-        words_col.append(entry[0])
-        counts_col.append(entry[1])
-    clustered_histogram = cluster_similar_expressions(cond_histogram, model, threshold)
-    clustered_histogram = sorted(clustered_histogram.items(), reverse=True, key=lambda t: t[1])
-    print(clustered_histogram)
-    with open(f"semantic_clusters_{condition}_{model_name}.tsv", "w") as f:
-        f.write(f"terms\tcount\n")
-        for item in clustered_histogram:
-            f.write(f"{item[0]}\t{item[1]}\n")
+# for condition, cond_list in matched_conditions.items():
+#     print(f"****{condition}****")
+#     cond_histogram = dict(collections.Counter(cond_list))
+#     words_col = []
+#     counts_col = []
+#     for entry in cond_histogram.items():
+#         words_col.append(entry[0])
+#         counts_col.append(entry[1])
+#     clustered_histogram = cluster_similar_expressions(cond_histogram, model, threshold)
+#     clustered_histogram = sorted(
+#         clustered_histogram.items(), reverse=True, key=lambda t: t[1]
+#     )
+#     print(clustered_histogram)
+#     with open(f"semantic_clusters_{condition}_{model_name}.tsv", "w") as f:
+#         f.write(f"terms\tcount\n")
+#         for item in clustered_histogram:
+#             f.write(f"{item[0]}\t{item[1]}\n")
