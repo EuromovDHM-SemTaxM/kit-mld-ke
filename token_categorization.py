8d9363b7a39c86620b8e2d6bc6ded0928b84e4fc
twktheainur <andon.tchechmedjiev@mines-ales.fr>
2023-08-08 11:55:19 +0200

    Refactored token classification script, added filters

--- token_categorization.py
+++ token_categorization.py
@@ -1,7 +1,9 @@
 import collections
 import json
+import sys
 
 from urllib.parse import urlencode
+from anyio import Path
 
 import numpy as np
 import pandas
@@ -9,10 +11,13 @@ import torch
 from torch import pdist, nn, mm
 
 from tqdm import tqdm
-
+import requests
 
 model_name = "paraphrase-multilingual-mpnet-base-v2"
 threshold = 0.2
+
+from pyclinrec.dictionary import MgrepDictionaryLoader
+from pyclinrec.recognizer import IntersStemConceptRecognizer
 
 from redis import Redis
 
@@ -40,59 +45,49 @@ def get(url: str, headers, timeout=None):
     return page_text
 
 
-def cluster_similar_expressions(ordered_dict, model, threshold=0.4):
-    terms = list(ordered_dict.keys())
-    counts = list(ordered_dict.values())
-    # Embedding
-    vectors = torch.Tensor(model.encode(terms))
-
-    # Computing cosine distance matrix
-    vectors = nn.functional.normalize(vectors, p=2, dim=-1)
-    dist = 1 - mm(vectors, vectors.t())
-
-    # Thresholding and keeping only upper triangle
-    mask = dist < threshold
-    dist = torch.triu(dist * mask.int().float())
-
-    # Clustering
-    already_in_cluster = set()
-    merged_ordered_dict = {}
-    for i in range(dist.shape[0]):
-        if i not in already_in_cluster:
-            indices = np.nonzero(dist[i])
-            new_key = ", ".join([terms[index] for index in indices])
-            total = 0
-            for index in indices:
-                index = index.item()
-                total += counts[index]
-                already_in_cluster.add(index)
-            merged_ordered_dict[new_key] = total
-    return merged_ordered_dict
+def parse_text(text):
+    url = f"http://localhost:8080/predict/semantics?{urlencode({'utterance': text})}"
+    response = get(url, {})
+    return json.loads(response) if response is not None else ""
 
 
-# sentences_df = pandas.read_csv(
-#     "extracted_knowlege/Gauss_303_.csv", sep=",", header=None
-# )
+def is_compositional(propositions):
+    return any(
+        propositions[i]["start"] != propositions[i + 1]["start"]
+        for i in range(len(propositions) - 1)
+    )
 
-# corpus = sentences_df.iloc[:, 0].tolist()
 
-with open("extracted_knowlege/h3D_preds_refs.csv", "r") as f:
-    lines = f.readlines()
-refs = []
-preds = []
+def is_successive(propositions):
+    overlapping_counter = sum(
+        propositions[i]["start"] == propositions[i + 1]["start"]
+        for i in range(len(propositions) - 1)
+    )
+    return overlapping_counter == 0
 
-for id, line in enumerate(lines):
-    rp = line.replace("\n", "").split(",")
-    local_refs = rp[1:]
-    pred = rp[0]
-    for i in range(len(local_refs)):
-        refs.append(local_refs[i])
-        preds.append(pred)
 
-corpus = preds
-import requests
+def create_part_annotator():
+    dictionary_loader = MgrepDictionaryLoader("resources/body_part_dictionary.tsv")
+    concept_recognizer = IntersStemConceptRecognizer(
+        dictionary_loader,
+        "resources/stopwords_en.txt",
+        "resources/termination_terms_en.txt",
+    )
 
-matched_conditions = {}
+    concept_recognizer.initialize()
+
+    return concept_recognizer
+
+
+def determine_composition_type(prop_annotations):
+    if len(prop_annotations) == 1:
+        return "single"
+    elif is_compositional(prop_annotations):
+        return "compositional"
+    elif is_successive(prop_annotations):
+        return "successive"
+
+
 span_conditions = {
     "agents": (lambda x: x["vn"] == "Agent", lambda x: x["text"]),
     "actions": (lambda x: x["vn"] == "Verb", lambda x: x["text"]),
@@ -104,67 +99,121 @@ span_conditions = {
         lambda x: x["text"],
     ),
 }
-limit = 20
-output_document = []
-for index, sentence in tqdm(list(enumerate(corpus))):
-    sentence = sentence.replace("<eos>", ".")
-    # print("Sentence: "+ sentence)
-    url = (
-        f"http://localhost:8080/predict/semantics?{urlencode({'utterance': sentence})}"
-    )
-    response = get(url, {})
-    if response is not None:
-        response = json.loads(response)
-        annotation = {}
-        senses = []
-        if "props" in response:
-            for prop in response["props"]:
-                if "performance" not in prop["sense"]:
-                    spans = prop["spans"]
-                    for span in spans:
-                        for condition, value in span_conditions.items():
-                            if value[0](span):
-                                if condition not in annotation:
-                                    annotation[condition] = []
 
-                                annotation[condition].append(
-                                    (
-                                        span_conditions[condition][1](span),
-                                        span["start"],
-                                        span["end"],
-                                    )
-                                )
-                    senses.append(prop["sense"])
-            output_document.append(
+
+def extract_prop_text(tokens, prop_start, prop_end):
+    prop_tokens = [token["text"] for token in tokens[prop_start : prop_end + 1]]
+    return " ".join(prop_tokens)
+
+
+def process_spans(spans):
+    annotation = {}
+    prop_start = sys.maxsize
+    prop_end = -1
+    for span in spans:
+        prop_start = min(span["start"], prop_start)
+        prop_end = max(span["end"], prop_end)
+        for condition, value in span_conditions.items():
+            if value[0](span):  # Check if span satisfies condition (call to lambda)
+                if condition not in annotation:
+                    annotation[condition] = []
+
+                annotation[condition].append(
+                    {
+                        "text": span_conditions[condition][1](span),
+                        "start": span["start"],
+                        "end": span["end"],
+                    }
+                )
+
+    return prop_start, prop_end, annotation
+
+
+def process_corpus(corpus):
+    annotator = create_part_annotator()
+
+    output_document = []
+    for index, sentence in tqdm(list(enumerate(corpus))):
+        sentences = sentence if isinstance(sentence, list) else [sentence]
+        index_output = []
+        for sentence in sentences:
+            response = parse_text(sentence.replace("<eos>", "."))
+            if not response or len(response) == 0 or "props" not in response:
+                output_document.append(
+                    {"sentence": sentence, "sentence_index": index, "propositions": []}
+                )
+                continue
+
+            tokens = response["tokens"]
+            prop_annotations = [
+                process_prop(annotator, tokens, prop)
+                for prop in response["props"]
+                if not excluded(prop)
+            ]
+
+            index_output.append(
                 {
                     "sentence": sentence,
                     "sentence_index": index,
-                    "senses": prop["sense"],
-                    "annotations": annotation,
+                    "propositions": prop_annotations,
+                    "composition": determine_composition_type(prop_annotations),
                 }
             )
+        output_document.append(index_output)
+    return output_document
 
-with open("extracted_knowlege/semantic_segmentation_jit.json", "w") as f:
-    json.dump(output_document, f, indent=2)
-    # limit -= 1
-    # if limit == 0:
-    #     break
-# model = SentenceTransformer(model_name)
 
-# for condition, cond_list in matched_conditions.items():
-#     print(f"****{condition}****")
-#     cond_histogram = dict(collections.Counter(cond_list))
-#     words_col = []
-#     counts_col = []
-#     for entry in cond_histogram.items():
-#         words_col.append(entry[0])
-#         counts_col.append(entry[1])
-#     clustered_histogram = cluster_similar_expressions(cond_histogram, model, threshold)
-#     clustered_histogram = sorted(
-#         clustered_histogram.items(), reverse=True, key=lambda t: t[1]
-#     )
-#     print(clustered_histogram)
-#     with open(f"semantic_clusters_{condition}_{model_name}.tsv", "w") as f:
-#         f.write(f"terms\tcount\n")
-#         for item in clustered_histogram:
-#             f.write(f"{item[0]}\t{item[1]}\n")
+def process_prop(annotator, tokens, prop):
+    spans = prop["spans"]
+
+    prop_start, prop_end, span_annotation = process_spans(spans)
+
+    prop_text = extract_prop_text(tokens, prop_start, prop_end)
+
+    _, _, annotations = annotator.match_mentions(prop_text)
+
+    span_annotation["body_parts"] = set()
+    for annotation in annotations:
+        span_annotation["body_parts"].add(annotation.concept_id)
+
+    span_annotation["body_parts"] = list(span_annotation["body_parts"])
+
+    return {
+        "start": prop_start,
+        "end": prop_end,
+        "text": prop_text,
+        "sense": prop["sense"],
+        "annotations": span_annotation,
+    }
+
+
+def excluded(prop):
+    return (
+        "performance" in prop["sense"]
+        or "hold" in prop["sense"]
+        or "seem" in prop["sense"]
+    )
+
+
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    stem = Path(input_file).stem
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+    refs = []
+    preds = []
+
+    for line in lines:
+        rp = line.replace("\n", "").split(",")
+        local_refs = rp[1:]
+        pred = rp[0]
+        refs.append(local_refs)
+        preds.append(pred)
+
+    preds_output = process_corpus(preds)
+    with open(f"{stem}_preds.json", "w") as f:
+        json.dump(preds_output, f, indent=2)
+
+    refs_output = process_corpus(refs)
+    with open(f"{stem}_refs.json", "w") as f:
+        json.dump(refs_output, f, indent=2)
